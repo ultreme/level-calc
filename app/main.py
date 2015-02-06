9f51a4624bb2edc7903d294a9bddcf75d498c5ad
s. rannou <mxs@sbrk.org>
2015-02-07 00:11:42 +0100

    Imported misc helpers, for more fun.

--- app/main.py
+++ app/main.py
@@ -1,19 +1,92 @@
+import datetime
+import random
 import sys
 
 from flask import Flask, render_template, redirect, url_for
 from flask.ext.script import Manager
 
 
+# Globals
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asd'
 app.config['DEBUG'] = True
 
+# Misc stuff
+def random_caps(s):
+    r = ''
+    for c in s:
+        if int(2 * random.random()) is 0:
+            r += c.capitalize()
+        else:
+            r += c
+    return r
 
+def megahertz():
+    return 100 * random.random()
+
+def yomyman_style():
+    styles = ['cachou', 'jambon', 'epinard', 'lasagne', 'haricot', 'sandwich']
+    return 'cool-style-' + styles[int(random.random() * len(styles))]
+
+def logo_alternate():
+    base_path = '/static/img/logo-alternate-300/'
+    images = [
+        '50-bacon-50-biere-50-camembert.png',
+        'dj-jean-michel-mc-pierre-gustave.jpg',
+        'i-love-chips.jpg',
+        'le-passe-c-etait-mieux-avant.jpg',
+        'c-est-vachement-cool.jpg',
+        'arc-en-le-ciel.jpg',
+        'aspiration.jpg',
+        'boules.jpg',
+        'etoile.jpg',
+        'megas-pixels.jpg',
+        'petits-pixels.jpg',
+        'pochette-camembert.jpg',
+        'pouce.jpg',
+        'trim.jpg',
+        'trop-de-la-balle.jpg',
+    ]
+    return base_path + images[int(random.random() * len(images))]
+
+def mot_du_jour():
+    day = int(datetime.datetime.now().strftime('%d'))
+    mots_du_jour = [
+        'une gomme', 'une pomme', 'phazms', '08 36 65 65 65', 'le mot du jour',
+        'cool', 'trop de la balle', 'coucou', 'salut les copains',
+        'un sandwich', 'caribou', 'jus', 'du pate', 'poney', 'castor',
+        'cancun', 'hula hoop', 'criterium', 'lampadaire', 'tabernacle',
+        'anticonstitutionnellement', 'grut', 'i feel good', 'cumulonumbus',
+        'chewing gum', 'jericane', 'malaxer', 'competant', 'moineau', 'wesh',
+        'platane', 'sycomore', 'blaireau', 'perudo', 'azymute',
+        'moissoneuse batteuse', 'tracteur', 'pudding', '1000 pates', '42',
+        'centipede',
+    ]
+    return random_caps(mots_du_jour[day % len(mots_du_jour)])
+
+def mot_debile_qui_se_mange():
+    mots_debiles = [
+        'beurre', 'lait', 'yahourt', 'pain', 'gruyere', 'margarine',
+        'curcumin', 'd\'epinard', 'banane', 'salade', 'brioche',
+        'sucre', 'chips', 'laitage', 'lukum', 'flotte', 'chupa-chups',
+        'yogourt',
+    ]
+    return mots_debiles[int(random.random() * len(mots_debiles))]
+
+app.jinja_env.globals.update(
+    yomyman_style=yomyman_style,
+    logo_alternate=logo_alternate,
+    mot_du_jour=mot_du_jour,
+    megahertz=megahertz,
+    mot_debile_qui_se_mange=mot_debile_qui_se_mange,
+)
+
+# Routes
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
+# Main
 if __name__ == "__main__":
     manager = Manager(app)
     manager.run()
